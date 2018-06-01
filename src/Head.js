36cfd0fbbd6873b05cc064860d2f75586b2505d6
kenedw <kenedwanderson@live.com>
2018-05-31 23:45:11 -0300

    ajuste do login -Limitação de entrada de CPF

--- src/Head.js
+++ src/Head.js
@@ -7,20 +7,20 @@ import {  Row, Col} from 'reactstrap';
 export class Head extends Component {
   render() {
     return (
-    <div className="App App-header">
-      <header>
-        <Row>
-          <img src={logo} className="App-logo" />
-          <img src={logoPGP} className="App-logoPGP" />
-          <div>
-          <h1 className="App-title Text-indent App-logo">Sistema de Avaliação de Desempenho</h1>
-          <p className="Text-indent App-logo">Universidade Federal da Paraiba</p>
-          <p className="Text-indent App-logo">Pró-Reitoria de Gestão de Pessoas</p>
-          <p className="Text-indent App-logo">Hospital Universitário Lauro Wanderley</p>
-          </div>
-        </Row>
-      </header>
-    </div>
+      <div className="App App-header">
+          <Row>
+            <div>
+              <h1 className="App-title Text-indent">Sistema de Avaliação de Desempenho</h1>
+              <p className="Text-indent">Universidade Federal da Paraiba</p>
+              <p className="Text-indent">Pró-Reitoria de Gestão de Pessoas</p>
+              <p className="Text-indent">Hospital Universitário Lauro Wanderley</p>
+            </div>
+            <div>
+              <img src={logoPGP} className="App-logoPGP" />
+              <img src={logo} className="App-logo" />
+            </div>
+          </Row>
+      </div>
     );
   }
 }
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -4,66 +4,72 @@ import newLogin from './actions/validation'
 
 class Login extends React.Component{
 
-    constructor(props){
-        super(props);
-        this.state            = {user: '', pass: ''};
-        this.handleChangeUser = this.handleChangeUser.bind(this);
-        this.handleChangePass = this.handleChangePass.bind(this);
-        this.handleSubmit     = this.handleSubmit.bind(this);
-        this.verificar        = this.verificar.bind(this);
+  constructor(props){
+    super(props);
+    this.state            = {user: '', pass: ''};
+    this.handleChangeUser = this.handleChangeUser.bind(this);
+    this.handleChangePass = this.handleChangePass.bind(this);
+    this.handleSubmit     = this.handleSubmit.bind(this);
+    this.verificar        = this.verificar.bind(this);
+  }
+
+  handleChangeUser(event) {
+    this.setState( {user: formatarCpf(event.target.value)} ); // atualiza o valor do cpf formatado
+  }
+
+  handleChangePass(event) {
+    this.setState({ pass: event.target.value });
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+  }
+
+  verificar(){
+    if(this.state.user === '' || this.state.pass === '' ){
+      alert('Por favor, preencha os campos!')
+    }
+    else{
+      if(!newLogin(this.state.user,this.state.pass)){
+        alert('Usuário e/ou senha digitados incorretamente!');
+        //
+      }
+      else{
+        window.open("/userpage","_self");
+      }
     }
 
-    handleChangeUser(event) {
-        this.setState({ user: event.target.value });
-    }
+  }
 
-    handleChangePass(event) {
-        this.setState({ pass: event.target.value });
-    }
+  render(){
+    return (
 
-    handleSubmit(event) {
-
-        event.preventDefault();
-    }
-
-    verificar(){
-        if(this.state.user === '' || this.state.pass === '' ){
-            alert('Por favor, preencha os campos!')
-        }
-        else{
-            if(!newLogin(this.state.user,this.state.pass)){
-                alert('Usuário e/ou senha digitados incorretamente!');
-                //
-            }
-            else{
-                window.open("/userpage","_self");
-            }
-        }
-
-    }
-
-    render(){
-        return (
-
+      <div>
+        <div id="login">
+          <form name='form-login' onSubmit={this.handleSubmit}>
+            <h3>Login</h3>
+            <span className="fontawesome-user"></span>
+            <input type="text-number" value={this.state.user} minLength='14' maxLength='14' placeholder="CPF" name="CPF" onChange={this.handleChangeUser}/>
+            <span className="fontawesome-lock"></span>
+            <input type="password" value={this.state.pass} placeholder="Senha" onChange={this.handleChangePass}/>
             <div>
-                <div id="login">
-                    <form name='form-login' onSubmit={this.handleSubmit}>
-                        <h3>Login</h3>
-                        <span className="fontawesome-user"></span>
-                        <input type="number" value={this.state.user} placeholder="CPF" onChange={this.handleChangeUser}/>
-                        <span className="fontawesome-lock"></span>
-                        <input type="password" value={this.state.pass} placeholder="Senha" onChange={this.handleChangePass}/>
-                        <div>
-                            <a href="/cadastrar">Criar Conta</a>
-                            <a href="/esqueciSenha">Esqueci a senha</a>
-                        </div>
-                        <input type="submit" value="Login" onClick={this.verificar} />
-                    </form>
-                </div>
+              <a href="/cadastrar">Criar Conta</a>
+              <a href="/esqueciSenha">Esqueci a senha</a>
             </div>
+            <input type="submit" value="Login" onClick={this.verificar} />
+          </form>
+        </div>
+      </div>
 
-        )
-    }
+    )
+  }
 }
-
+function formatarCpf(cpf){
+  cpf = cpf.replace(/\D/g,"");
+  cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
+  cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
+  cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+  return cpf;
+}
 export default Login
