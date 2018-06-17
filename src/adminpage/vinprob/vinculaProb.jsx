ef3c7347029118a6f60f3bd94df0a455a4bf681e
Kened Oliveira <kenedwanderson@live.com>
2018-06-17 20:47:50 -0300

    Correção do token

--- src/adminpage/vinprob/vinculaProb.jsx
+++ src/adminpage/vinprob/vinculaProb.jsx
@@ -5,12 +5,15 @@ import axios from 'axios';
 import Lista from './probList';
 import Decodificar from '../decodifica';
 var token = {
-  'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIsImNwZiI6IjEwNDEwNDEwNDEwIiwiaWF0IjoxNTI5MjQ3Mjk4LCJleHAiOjE1MjkzMzM2OTh9.l9xtUlHBBn6sgXbNB5Gm_YIzfwk096h27nYNmSRVJCE',
-  // 'Content-Type':'application/json',
-  // 'Accept':'application/json',
+  headers:
+  { 'postman-token': '9e60e149-eae0-183f-7716-ded2c58b4471',
+    'cache-control': 'no-cache',
+    'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIsImNwZiI6IjEwNDEwNDEwNDEwIiwiaWF0IjoxNTI5MjQ3Mjk4LCJleHAiOjE1MjkzMzM2OTh9.l9xtUlHBBn6sgXbNB5Gm_YIzfwk096h27nYNmSRVJCE',
+    accept: 'application/json',
+    'content-type': 'application/json'
+  }
 };
-// const instance = axios.create();
-// instance.defaults.headers.common['Authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIsImNwZiI6IjEwNDEwNDEwNDEwIiwiaWF0IjoxNTI5MjQ3Mjk4LCJleHAiOjE1MjkzMzM2OTh9.l9xtUlHBBn6sgXbNB5Gm_YIzfwk096h27nYNmSRVJCE';
+
 var clickInfo = false;
 class admin extends Component {
   constructor(){
@@ -77,43 +80,43 @@ class admin extends Component {
                       var result = "@";
                       if(this.state.cpf.length == 14){result ='cpf/' + cpf2int(this.state.cpf);}
                       else if(this.state.dataProb.length == 10){result = 'data/' + this.state.dataProb;}
-                        console.log(token);
-                        axios.get('https://hulw.herokuapp.com/usuario/' + result, 'NULL',token )
-                        .then((response) => {
-                          console.log(response);
-                          this.setState({response: response.data});
-                          this.setState({open: true });
-                        }).catch((error) => {
-                          console.log(error);
-                          this.setState({open: false });
-                        });
-                        clickInfo = true;
-                      }}>Pesquisar</Button>
-                    </Form>
-                  </div>
-                  <Collapse isOpen={this.state.open}>
-                    {clickInfo === true && <div> <Lista list={this.state.response}/> </div>}
-                  </Collapse>
-                </CardBody>
-              </Card>
-            </div>
+                      console.log(token);
+                      axios.get('https://hulwteste.herokuapp.com/usuario/' + result, token )
+                      .then((response) => {
+                        console.log(response);
+                        this.setState({response: response.data});
+                        this.setState({open: true });
+                      }).catch((error) => {
+                        console.log(error);
+                        this.setState({open: false });
+                      });
+                      clickInfo = true;
+                    }}>Pesquisar</Button>
+                  </Form>
+                </div>
+                <Collapse isOpen={this.state.open}>
+                  {clickInfo === true && <div> <Lista list={this.state.response}/> </div>}
+                </Collapse>
+              </CardBody>
+            </Card>
           </div>
-        )
-      }
+        </div>
+      )
     }
+  }
 
-    function cpf2int(cpf){
-      cpf = cpf.replace(/[^0-9]+/g,'');
+  function cpf2int(cpf){
+    cpf = cpf.replace(/[^0-9]+/g,'');
 
-      return cpf;
-    }
+    return cpf;
+  }
 
 
-    function formatarCpf(cpf){
-      cpf = cpf.replace(/\D/g,"");
-      cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
-      cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
-      cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
-      return cpf;
-    }
-    export default admin;
+  function formatarCpf(cpf){
+    cpf = cpf.replace(/\D/g,"");
+    cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+    return cpf;
+  }
+  export default admin;
