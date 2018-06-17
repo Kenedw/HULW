8f5c7abec37b346c8b4d0b1732ed1fb3add8627d
Unknown <danelias180@gmail.com>
2018-06-17 20:52:05 -0300

    Avaliacao Probatorio

--- src/MainRoute.jsx
+++ src/MainRoute.jsx
@@ -37,7 +37,7 @@ class Rota extends Component {
             <Route path="/esqueciSenha"  component = {esqueci} />
             <Route path="/userpage"      component = {Userpage} />
             <Route path="/avaliacao"     component = {Aval}/>
-            <Route path="/avalprob"      component = {Aval2}/>
+            <Route path="/avaliacaoprob" component = {Aval2}/>
             <Route path="/cadastroTec"   component = {Cadastro} />
             <Route path="/unidade"       component = {Unidade} />
             <Route path="/pesqUnidade"   component = {PesqUnidade}/>
