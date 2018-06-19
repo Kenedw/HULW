f8b6e5e79b5aa49fcbf1add904ca652cb83d24f2
Kened Oliveira <kenedwanderson@live.com>
2018-06-19 08:18:21 -0300

    Melhoria em vincular probatorio
    Correção do link da cloud do backend
    adição do campo data do probatório
    adição dos métodos de inserção e delete da tabela

--- src/adminpage/vinprob/probList.jsx
+++ src/adminpage/vinprob/probList.jsx
@@ -1,25 +1,65 @@
 import React from 'react'
 import { Button, Table, Input, CustomInput} from 'reactstrap';
+import axios from 'axios';
+
+const URL = 'https://hulw.herokuapp.com/';
 
 export default props => {
-
-  var list = props.list;
-  var cpfAdmin = props.cpfAdmin;
+  var CheckProb = true;
+  var list      = props.list;
+  var cpfAdmin  = props.cpfAdmin;
   if(!props.list.length){
     list = [props.list];
   }
-
+  //captura o estado inicial do checkbox
+  function init_checkbox(){
+    console.log("iniciado");
+    axios.get(`${URL}estagio/probatorio/usuario/` + list.id_Usuario, props.token )
+    .then((response) => {//se ela existir na tabela, ela esta em estado probatorio
+      console.log(response);
+      list.id_Estagio_Probatorio = response.id_Estagio_Probatorio;
+      this.CheckPro = true;
+    }).catch((error) => {//se não existir é a primeira vez
+      console.log(error);
+      this.CheckPro = false;
+    });
+    console.log(list.id_Estagio_Probatorio);
+  }
   const renderRows = () =>{
     return list.map(todo =>(
       <tr key={todo.id_Usuario}>
-          <td >{todo.no_Pessoa}</td>
-          <td >{todo.cd_CPF}</td>
-          <td >{todo.cd_Email}</td>
-          <td>
-            <Input type="checkbox" value={todo.id_Usuario}  checked={todo.probatorio}   />
-          </td>
+        <td>{todo.no_Pessoa}</td>
+        <td>{todo.cd_CPF}   </td>
+        <td>{todo.cd_Email} </td>
+        <td>{props.dataProb.length!==0?props.dataProb:todo.dt_Admissao.substring(0,4)}</td>
+        <td>
+          <Input type="checkbox" name={todo.id_Usuario}  checked={this.CheckProb}  onChange={handleInputChange(todo)} />
+        </td>
       </tr>
     ))
+  }
+  function handleInputChange(cha) {
+    var data = {'dt_Ano':function(){props.dataProb.length!==0?props.dataProb:cha.dt_Admissao.substring(0,4)},'id_Usuario':cha.id_Usuario}
+    data = JSON.stringify(data);
+    console.log(data);
+    if(CheckProb === false){
+      CheckProb = true;
+      axios.post(`${URL}estagio/estagio/probatorio/`, data, props.token )
+      .then((response) => {//se ela existir na tabela, ela esta em estado probatorio
+        console.log(response.data);
+        this.CheckPro = true;
+      }).catch((error) => {
+        console.log(error);
+        this.CheckPro = false;
+      });
+    }else{
+      axios.delete(`${URL}estagio/estagio/probatorio/` + cha.id_Estagio_Probatorio, props.token )
+      .then((response) => {
+        console.log(response);
+        this.CheckPro = false;
+      });
+      CheckProb = false;
+    }
   }
 
   return(
@@ -29,16 +69,14 @@ export default props => {
           <th> Nome</th>
           <th> CPF</th>
           <th> E-Mail</th>
+          <th> Ano</th>
           <th> Estagio Probatorio</th>
         </tr>
       </thead>
       <tbody>
-      {renderRows()}
+        {init_checkbox()}
+        {renderRows()}
       </tbody>
     </Table>
   )
-}
-
-function handleInputChange(event){
-
 }
--- src/adminpage/vinprob/vinculaProb.jsx
+++ src/adminpage/vinprob/vinculaProb.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Lista from './probList';
 import Decodificar from '../decodifica';
 
-const URL = 'https://hulwteste.herokuapp.com/';
+const URL = 'https://hulw.herokuapp.com/';
 var token = {
   headers:
   {
@@ -24,23 +24,24 @@ class admin extends Component {
       cpf: "",
       open: false,
       response: [],
-      dataProb: ""
+      dataProb: "",
+      dataPesq: "",
+      // CheckProb: false,
     };
-    this.handleInputChange = this.handleInputChange.bind(this);
+    // this.handleInputChange = this.handleInputChange.bind(this);
 
     this.onChange = (evento) => {
       this.setState({nome: evento.target.value});
       const state = Object.assign({}, this.state);
       const campo = evento.target.name;
-
       state[campo] = evento.target.value;
 
       this.setState(state);
       if(campo === 'cpf'){
         this.setState( {cpf: formatarCpf(evento.target.value)} ); // atualiza o valor do cpf formatado
       }
-      if(campo === 'dataProb'){
-        this.setState( {dataProb: evento.target.value} );
+      if(campo === 'dataPesq'){
+        this.setState( {dataPesq: evento.target.value} );
       }
     };
   }
@@ -59,9 +60,14 @@ class admin extends Component {
     this.pegaDados();
   }
 
-  handleInputChange(event) {
-    console.log("oioioioioioi");
-  }
+  // handleInputChange() {
+  //   if(this.state.CheckProb === false){
+  //     this.setState({CheckProb:true});
+  //   }else{
+  //     this.setState({CheckProb:false});
+  //   }
+  //
+  // }
 
   render(){
     return (
@@ -72,19 +78,22 @@ class admin extends Component {
               <h3>Vincular ao periodo probatorio</h3>
               <div className="form-group">
                 <p></p>
-                <label>Pesquisar por CPF ou DATA de admissão: </label>
+                <label>Pesquisar por CPF ou DATA de admissão, caso necessário use a data do probatorio(opcional)</label>
                 <Form inline>
-                  <Input type="text" className="form-control" name="cpf"
-                    value={this.state.cpf} onChange={this.onChange} placeholder="000.000.000-00"
+                  <Input type="text" className="form-control a-fix" name="cpf"
+                    value={this.state.cpf} onChange={this.onChange} placeholder="CPF"
                     minLength='14' maxLength='14'/>
-                  <Input type="date" name="dataProb" placeholder="date placeholder" value={this.state.dataProb} onChange={this.onChange}/>
+                  <Input type="date" className="form-control a-fix" name="dataPesq" placeholder="date placeholder" alt="data de admissão" value={this.state.dataPesq} onChange={this.onChange}/>
+                  {/*JSON.stringify(this.state) DEBUG*/}
+                  <Input type="text"className="form-control a-fix" name="dataProb" placeholder="YYYY" maxLength="4" value={this.state.dataProb} onChange={this.onChange}/>
                   <Button outline className="a-fix" onClick={()=>{
                       var result = "@";
                       if(this.state.cpf.length == 14){result ='cpf/' + cpf2int(this.state.cpf);}
-                      else if(this.state.dataProb.length == 10){result = 'data/' + this.state.dataProb;}
+                      else if(this.state.dataPesq.length == 10){result = 'data/' + this.state.dataPesq;}
                       axios.get(`${URL}usuario/` + result, token )
                       .then((response) => {
-                        this.setState({response: response.data});
+                        console.log(response.data);
+                        this.setState({response:response.data});
                         this.setState({open: true });
                       }).catch((error) => {
                         console.log(error);
@@ -95,7 +104,7 @@ class admin extends Component {
                   </Form>
                 </div>
                 <Collapse isOpen={this.state.open}>
-                  {clickInfo === true && <div> <Lista list={this.state.response}/> </div>}
+                  {clickInfo === true && <div> <Lista list={this.state.response} dataProb={this.state.dataProb} token={token}/> </div>}
                 </Collapse>
               </CardBody>
             </Card>
@@ -119,4 +128,5 @@ class admin extends Component {
     cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
     return cpf;
   }
+
   export default admin;
